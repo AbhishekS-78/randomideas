dcf1c0ccef8e249e94c23c3149c3ad9de4eb90e6
AbhishekS-78 <thisisabhishek.78@gmail.com>
2026-06-06 11:17:30 +0530

    Add delete functionality

--- client/src/components/IdeaList.js
+++ client/src/components/IdeaList.js
@@ -16,6 +16,16 @@ class IdeaList {
     this._validTags.add("inventions");
   }
 
+  addEventListeners() {
+    this._ideaListEl.addEventListener("click", (e) => {
+      if (e.target.classList.contains("fa-times")) {
+        e.stopImmediatePropagation();
+        const ideaId = e.target.parentElement.parentElement.dataset.id;
+        this.deleteIdea(ideaId);
+      }
+    });
+  }
+
   async getIdeas() {
     try {
       const res = await IdeasApi.getIdeas();
@@ -23,6 +33,17 @@ class IdeaList {
       this.render();
     } catch (error) {
       console.log(error);
+    }
+  }
+
+  async deleteIdea(ideaId) {
+    try {
+      // Delete from server
+      const res = await IdeasApi.deleteIdea(ideaId);
+      this._ideas.filter((idea) => idea._id !== ideaId);
+      this.getIdeas();
+    } catch (error) {
+      alert("You cannot delete this resource");
     }
   }
 
@@ -48,9 +69,13 @@ class IdeaList {
     this._ideaListEl.innerHTML = this._ideas
       .map((idea) => {
         const tagClass = this.getTagClass(idea.tag);
+        const deletBtn =
+          idea.username === localStorage.getItem(`username`)
+            ? `<button class="delete"><i class="fas fa-times"></i></button>`
+            : "";
         return `
-        <div class="card">
-          <button class="delete"><i class="fas fa-times"></i></button>
+        <div class="card" data-id="${idea._id}">
+        ${deletBtn}
           <h3>
             ${idea.text}
           </h3>
@@ -63,6 +88,7 @@ class IdeaList {
         `;
       })
       .join("");
+    this.addEventListeners();
   }
 }
 
